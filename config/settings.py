b998d50fd40fc3390b614bafb3876d85279b3a99
Ali Mashaghi <aliu.mashaghi@gmail.com>
2026-02-27 12:34:14 +0330

    Update settings.pys

--- config/settings.py
+++ config/settings.py
@@ -129,10 +129,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
-
-# SMS Configuration (sms.ir)
-SMS_API_KEY = os.getenv('SMS_API_KEY')
-SMS_LINE_NUMBER = os.getenv('SMS_LINE_NUMBER')
+EMAIL_HOST_USER = 
+EMAIL_HOST_PASSWORD = 
+DEFAULT_FROM_EMAIL = 
